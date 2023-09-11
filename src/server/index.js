4be1663c87046c5dfa230999c165580d9f1bbef5
ishan <ishannagpal554@gmail.com>
2023-09-10 20:46:31 -0500

    Integrated Mongodb
    to store user's top songs and artists on each login

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -13,9 +14,60 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 const FRONTEND_URI = process.env.FRONTEND_URI;
 const PORT = process.env.PORT || 8888;
+const MONGO_URI = process.env.MONGO_URI;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+let User;
+
+const artistSchema = new mongoose.Schema({
+    name: String,
+    rank: Number
+});
+
+const songSchema = new mongoose.Schema({
+    name: String,
+    rank: Number
+});
+
+const timeRangeDataSchema = new mongoose.Schema({
+    short_term: {
+        artists: [artistSchema],
+        songs: [songSchema]
+    },
+    medium_term: {
+        artists: [artistSchema],
+        songs: [songSchema]
+    },
+    long_term: {
+        artists: [artistSchema],
+        songs: [songSchema]
+    }
+});
+
+const loginSchema = new mongoose.Schema({
+    timestamp: Date,
+    data: timeRangeDataSchema
+});
+
+const userSchema = new mongoose.Schema({
+    spotifyId: String,
+    name: String,
+    logins: [loginSchema]
+});
+
+if (mongoose.models.User) {
+    User = mongoose.model('User');
+} else {
+    User = mongoose.model('User', userSchema);
+}
 
 
 const app = express();
@@ -23,98 +75,148 @@ const app = express();
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
 const generateRandomString = length => {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 };
-
 
 const stateKey = 'spotify_auth_state';
 
 app.get('/login', (req, res) => {
-  const state = generateRandomString(16);
-  res.cookie(stateKey, state);
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
 
-  const scope = [
-    'user-read-private',
-    'user-read-email',
-    'user-top-read',
-    'user-read-recently-played',
-    'user-follow-read',
-    'user-follow-modify',
-    'playlist-read-private',
-    'playlist-read-collaborative',
-    'playlist-modify-public',
-    'user-library-read',
-    'user-read-playback-position',
-    'user-library-read'
+    const scope = [
+        'user-read-private',
+        'user-read-email',
+        'user-top-read',
+        'user-read-recently-played',
+        'user-follow-read',
+        'user-follow-modify',
+        'playlist-read-private',
+        'playlist-read-collaborative',
+        'playlist-modify-public',
+        'user-library-read',
+        'user-read-playback-position',
+        'user-library-read'
     ].join(' ');
 
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        state: state,
+        scope: scope,
+    });
 
-
-  const queryParams = querystring.stringify({
-    client_id: CLIENT_ID,
-    response_type: 'code',
-    redirect_uri: REDIRECT_URI,
-    state: state,
-    scope: scope,
-  });
-
-  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-
-app.get("/callback", (req, res) => {
-    // This happens after the login route is successful, we get the authorization code first using the req.query.code and exchange it for an access token.
-    const code = req.query.code || null;
-
-    // Next is to setup a post req to the url endpoint for converting the code to an access token
-    // where we pass in the following params (querystring)
-    axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        data: querystring.stringify({
-            code: code,
-            redirect_uri: REDIRECT_URI,
-            grant_type: 'authorization_code'
-        }),
+const getUsersTopArtistsByRange = async (range, access_token) => {
+    return axios.get(`https://api.spotify.com/v1/me/top/artists?time_range=${range}&limit=50`, {
         headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
-        },
-    })
-        .then(response => {
-            if (response.status === 200) {
-                // If successful, then data will be returned which includes, access and refresh token, token type, token expiry in 3600 seconds, and the scope
-                const { access_token, refresh_token, expires_in } = response.data;
+            'Authorization': `Bearer ${access_token}`
+        }
+    });
+};
 
-                // pass along the tokens as params
-                const queryParams = querystring.stringify({
-                    access_token,
-                    refresh_token,
-                    expires_in
-                })
+const getUsersTopTracksByRange = async (range, access_token) => {
+    return axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${range}&limit=50`, {
+        headers: {
+            'Authorization': `Bearer ${access_token}`
+        }
+    });
+};
 
-                // redirect to react app
-                res.redirect(`${FRONTEND_URI}?${queryParams}`)
-            } else {
-                res.redirect(`/?${querystring.stringify({ error: 'invalid token' })}`);
+app.get("/callback", async (req, res) => {
+    const code = req.query.code || null;
+    const timeRanges = ['short_term', 'medium_term', 'long_term'];
+
+    try {
+        const response = await axios({
+            method: 'post',
+            url: 'https://accounts.spotify.com/api/token',
+            data: querystring.stringify({
+                code: code,
+                redirect_uri: REDIRECT_URI,
+                grant_type: 'authorization_code'
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+            },
+        });
+
+        if (response.status === 200) {
+            const { access_token, refresh_token, expires_in } = response.data;
+
+            // Fetch user's Spotify profile using the access token
+            const userProfile = await axios.get('https://api.spotify.com/v1/me', {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                }
+            });
+
+            const spotifyId = userProfile.data.id;
+            const name = userProfile.data.display_name;
+
+            const currentLoginData = {
+                timestamp: new Date(),
+                data: {
+                    short_term: {},
+                    medium_term: {},
+                    long_term: {}
+                }
+            };
+
+            for (const range of timeRanges) {
+                const topArtistsResponse = await getUsersTopArtistsByRange(range, access_token);
+                const topTracksResponse = await getUsersTopTracksByRange(range, access_token);
+
+                currentLoginData.data[range].artists = topArtistsResponse.data.items.map((artist, index) => ({ 
+                    name: artist.name, 
+                    rank: index + 1
+                }));
+
+                currentLoginData.data[range].songs = topTracksResponse.data.items.map((track, index) => ({ 
+                    name: track.name, 
+                    rank: index + 1
+                }));
             }
-        })
-        .catch(err => {
-            res.send(err)
-            console.log(err)
-        })
-})
+
+            const existingUser = await User.findOne({ spotifyId: spotifyId });
+
+            if (existingUser) {
+                existingUser.logins.push(currentLoginData);
+                await existingUser.save();
+            } else {
+                const newUser = new User({
+                    spotifyId: spotifyId,
+                    name: name,
+                    logins: [currentLoginData]
+                });
+                await newUser.save();
+            }
+
+            // Continue with your existing redirect logic
+            const queryParams = querystring.stringify({
+                access_token,
+                refresh_token,
+                expires_in
+            });
+            res.redirect(`${FRONTEND_URI}?${queryParams}`);
+        } else {
+            res.redirect(`/?${querystring.stringify({ error: 'invalid token' })}`);
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 
 app.get('/refresh_token', (req, res) => {
@@ -145,9 +247,8 @@ app.get('/refresh_token', (req, res) => {
         });
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Express app listening at http://localhost:${PORT}`);
+    console.log(`Express app listening at http://localhost:${PORT}`);
 });
 
 export { app };
